c6d450f9964864b39c1c6d8a06a4a3a1d2a56d90
Heeal <sevilla@et.esiea.fr>
2020-04-02 09:11:18 +0200

    [Features] Add the counter button

--- app/src/main/java/com/example/td1/FirstFragment.java
+++ app/src/main/java/com/example/td1/FirstFragment.java
@@ -1,9 +1,12 @@
 package com.example.td1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -11,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 public class FirstFragment extends Fragment {
+    TextView showCountTextView;
 
     @Override
     public View onCreateView(
@@ -18,7 +22,18 @@ public class FirstFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View fragmentFirstLayout = inflater.inflate(R.layout.fragment_first, container, false);
+        // Get the count text view
+        showCountTextView = fragmentFirstLayout.findViewById(R.id.textview_first);
+
+        return fragmentFirstLayout;
+    }
+
+    private void countMe(View view){
+        String countString = showCountTextView.getText().toString();
+        Integer count = Integer.parseInt(countString);
+        count++;
+        showCountTextView.setText(count.toString());
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -27,8 +42,8 @@ public class FirstFragment extends Fragment {
         view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                TextView showCountTextView = view.getRootView().findViewById(R.id.textview_first);
+                int currentCount = Integer.parseInt(showCountTextView.getText().toString());
             }
         });
 
@@ -37,6 +52,12 @@ public class FirstFragment extends Fragment {
             public void onClick(View view) {
                 Toast myToast = Toast.makeText(getActivity(), "Hello toast!", Toast.LENGTH_SHORT);
                 myToast.show();
+            }
+        });
+        view.findViewById(R.id.add_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                countMe(view);
             }
         });
     }
